8c1e9bb70908ccf8e0e7b922e6c5306a1da3ffbc
tomi77 <tomislav-ante.brncic@fer.hr>
2018-10-13 21:53:14 +0200

    Dodan Google login

---
+++ MoWizz/MoWizz/Scripts/GoogleAuthentication.js
@@ -0,0 +1,46 @@
+﻿/// <reference path="jquery-1.10.2.min.js" />
+
+function getAccessToken() {
+    if (location.hash) {
+        if (location.hash.split('access_token=')) {
+            var accessToken = location.hash.split('access_token=')[1].split('&')[0];
+            if (accessToken) {
+                isUserRegistered(accessToken);
+            }
+        }
+    }
+}
+
+function isUserRegistered(accessToken) {
+    $.ajax({
+        url: 'api/Account/UserInfo',
+        method: 'GET',
+        headers: {
+            'content-type': 'application/json',
+            'Authorization': 'Bearer ' + accessToken
+        },
+        success: function (response) {
+            if (response.HasRegistered) {
+                localStorage.setItem('accessToken', accessToken);
+                localStorage.setItem('userName', response.Email);
+                window.location.href = "Index.html";
+            } else {
+                signupExternalUser(accessToken);
+            }
+        }
+    });
+}
+
+function signupExternalUser(accessToken) {
+    $.ajax({
+        url: 'api/Account/RegisterExternal',
+        method: 'POST',
+        headers: {
+            'content-type': 'application/json',
+            'Authorization': 'Bearer ' + accessToken
+        },
+        success: function (response) {
+            window.location.href = "/api/Account/ExternalLogin?provider=Google&response_type=token&client_id=self&redirect_uri=http%3A%2F%2Flocalhost%3A49891%2FLogin.html&state=0M3Y_Vcd9josAb6LHqta_D3x8GkK0R2TSoDFaaDmowM1";
+        }
+    });
+}
